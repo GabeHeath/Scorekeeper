3cbc6357d63f30bf7f8ac34d8c6277e9c2c2d89a
Gabe Heath <gabe.d.heath@gmail.com>
2015-02-05 23:20:31 -0500

    A lot of homepage and nav bar fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,5 @@ class ApplicationController < ActionController::Base
             devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
             devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
         end
+
 end
